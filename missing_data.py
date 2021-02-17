8f6ba63a618a4e285f51d7e8e5dc6c2ae029c667
Ahmad Khabib <32435696+akhabibd@users.noreply.github.com>
2021-02-17 00:56:05 -0800

    Create missing_data.py

---
+++ missing_data.py
@@ -0,0 +1,11 @@
+# GPreda, missing data
+def missing_data(df):
+    total = df.isnull().sum()
+    percent = (df.isnull().sum()/df.isnull().count()*100)
+    tt = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
+    types = []
+    for col in df.columns:
+        dtype = str(df[col].dtype)
+        types.append(dtype)
+    tt['Types'] = types
+    return(np.transpose(tt))
