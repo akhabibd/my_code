6c11b3e5b695f2d53368079d26335947d462a7fa
Ahmad Khabib <32435696+akhabibd@users.noreply.github.com>
2021-03-07 00:19:48 -0800

    Update find_email.py

--- find_email.py
+++ find_email.py
@@ -1,10 +1,9 @@
 import re
-#regex for email
-email = re.compile(r'([A-Za-z0-9!#$%&\'*+-/_?^{}|~\.]+@(?:[A-Za-z0-9-]+\.)+([A-Za-z0-9-])+)') 
 
-list = []
+email = re.compile(r'([A-Za-z0-9!#$%&*+-/=?^_`{|}~\.]+@(?:[A-Za-z0-9-]+\.)+[A-Za-z0-9-]+)') 
+
+output = []
 for i in range(int(input())):
   for x in email.findall(input()):
-    list.append(x)
-    
-print(';'.joint(sorted(list(set(list)))))
+    output.append(x)
+print(';'.join(sorted(list(set(output)))))
